d91a1a151ae5d42310147eb5e80f3a338f35e7d3
imtiaz-m <61225374+imtiaz-m@users.noreply.github.com>
2020-03-21 13:00:19 +0600

    null vs undefind

---
+++ null vs-undefind.js
@@ -0,0 +1,18 @@
+//1
+let pakhi;
+function add(num1,num2){
+console.log(num1+num2);
+return
+
+}
+//2
+function add (num1,num2){
+    console.log(num1,num2);
+}
+//3
+const premik = {name:"smart dude", phone:6546562};
+console.log(premik.gf);
+ //4
+let fun = undefined;
+console.log(fun);
+
