212f05635c076889bfe99fc63e38c7b8050df749
imtiaz-m <61225374+imtiaz-m@users.noreply.github.com>
2020-03-21 12:34:57 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,38 @@
+// const age = 5;
+
+
+
+// if(age>0){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+//
+ const age = 0;
+
+
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+//
+const name = "0";
+
+
+if(name.length>0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false");
+}
+//FALSy
+//0
+//""
+//undefind
+//null
+//NaN
+//false
